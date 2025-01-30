019030fc617932f875b538589a4ff875644bd27f
천영성 <hokoro123@gmail.com>
2025-01-30 16:15:30 +0900

    CheonBlog 세팅

---
+++ src/main/java/com/example/spring/cheonblog/CheonBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.cheonblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheonBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheonBlogApplication.class, args);
+	}
+
+}
