64979559631f36268c4cf544ba3b7b092f9df0c8
천영성 <hokoro123@gmail.com>
2025-02-09 18:55:22 +0900

    이메일 형식 인증 로직 추가

--- cheonblogapp/src/Component/SignUp.jsx
+++ cheonblogapp/src/Component/SignUp.jsx
@@ -2,7 +2,16 @@ import React ,{useState} from 'react';
 import {useNavigate} from "react-router-dom";
 
 
+function validationEmail(email){    // 이메일 규정에 맞는지 체크
+    const regax =/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return regax.test(email);
+}
+
+
 function SignUp(){
+    const navigate = useNavigate(); // 회원가입 성공시 메인 홈페이지나 로그인 페이지로 랜더링
+    const [emailValid , setEmailValid] = useState(false);
+    const [emailMessage , setEmailMessage] = useState("");
     const [formData , setFormData] = useState({
         name:"",
         email:"",
@@ -15,6 +24,15 @@ function SignUp(){
             ...prevData,
             [name] : value
         }));
+        if(name==='email'){
+            if(validationEmail(value)){
+                setEmailValid(true);
+                setEmailMessage("이메일 형식이 일치합니다.");
+            }else{
+                setEmailValid(false);
+                setEmailMessage("");
+            }
+        }
 
 
     };
@@ -22,6 +40,7 @@ function SignUp(){
     return(
         <div className="SignUp">
             <p className="text-gray-500 p-3">회원가입 후 서비스 이용이 가능합니다.</p>
+            {emailValid && <p className="text-gray-500 p-3">{emailMessage}</p> }
             <form className="m-[2rem]">
                 <div className="m-[2rem]">
                     <input type="text" name="name" value={formData.name} onChange={handleChange} placeholder="이름"
@@ -35,7 +54,7 @@ function SignUp(){
                     <input type="password" name="password" value={formData.password} onChange={handleChange} placeholder="비밀번호" className="border p-[1rem] rounded-2xl w-[300px]"/>
                 </div>
                 <div className="m-[2rem]">
-                    <button type="submit" className="border rounded-[2rem] w-[150px] bg-black text-white">회원가입</button>
+                    <button type="submit" className="border rounded-[2rem] w-[150px] h-[40px] bg-black text-white hover:text-orange-500">SignUp</button>
                 </div>
 
             </form>
