fd2a11882c76a0b5f10e6a4f1452896ed0106811
천영성 <hokoro123@gmail.com>
2025-02-11 15:09:42 +0900

    회원가입 페이지 수정 - 이메일 인증 + 이름 + 비밀번호 모든 인증이 완료될 경우 회원가입 버튼 활성화

--- cheonblogapp/src/Component/SignUp.jsx
+++ cheonblogapp/src/Component/SignUp.jsx
@@ -7,15 +7,26 @@ function validationEmail(email){    // 이메일 규정에 맞는지 체크
     return regax.test(email);
 }
 
+function validationPassword(password){
+    const regax = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$/;
+    return regax.test(password);
+}
+
 
 function SignUp(){
     const navigate = useNavigate(); // 회원가입 성공시 메인 홈페이지나 로그인 페이지로 랜더링
-    const [emailValid , setEmailValid] = useState(false);
-    const [emailMessage , setEmailMessage] = useState("");
+    const [nameValid , setNameValid] = useState(false); // 이름에 입력 여부
+    const [emailValid , setEmailValid] = useState(false);   // 이메일 규정에 어긋나지 않았는지
+    const [emailCheckValid , setEmailCheckValid] = useState(false); // 이메일 인증 여부
+    const [passwordValid , setPasswordValid] = useState(false); // 패스워드 규정이 어긋나지 않았는지
+    const [passwordCheckValid , setPasswordCheckValid] = useState(false); // 비밀번호 확인이 맞는지
+    const [emailMessage , setEmailMessage] = useState("");  // 이메일 규정에 어긋날 경우 뜨는 메세지
+
     const [formData , setFormData] = useState({
         name:"",
         email:"",
-        password:""
+        password:"",
+        passwordCheck:""
     })
 
     const handleChange = (event) =>{
@@ -24,6 +35,13 @@ function SignUp(){
             ...prevData,
             [name] : value
         }));
+        if(name === 'name'){
+            if(value.length >= 2){
+                setNameValid(true);
+            }else{
+                setNameValid(false);
+            }
+        }
         if(name==='email'){
             if(validationEmail(value)){
                 setEmailValid(true);
@@ -31,6 +49,20 @@ function SignUp(){
             }else{
                 setEmailValid(false);
                 setEmailMessage("");
+            }
+        }
+        if(name==='password'){
+            if(validationPassword(value)){
+                setPasswordValid(true);
+            }else{
+                setPasswordValid(false);
+            }
+        }
+        if(name==='passwordCheck'){
+            if(value === formData.password){
+                setPasswordCheckValid(true);
+            }else{
+                setPasswordCheckValid(false);
             }
         }
 
@@ -48,15 +80,31 @@ function SignUp(){
                 </div>
                 <div className="m-[2rem]">
                     <input type="email" name="email" value={formData.email} onChange={handleChange} placeholder="이메일"
-                           className="border p-[1rem] rounded-2xl w-[300px]"/>
+                           className="border p-[1rem] rounded-2xl w-[300px] pr-[1rem]"/>
                 </div>
                 <div className="m-[2rem]">
-                    <input type="password" name="password" value={formData.password} onChange={handleChange} placeholder="비밀번호" className="border p-[1rem] rounded-2xl w-[300px]"/>
+                    <input type="password" name="password" value={formData.password} onChange={handleChange}
+                           placeholder="비밀번호" className="border p-[1rem] rounded-2xl w-[300px]"/>
                 </div>
                 <div className="m-[2rem]">
-                    <button type="submit" className="border rounded-[2rem] w-[150px] h-[40px] bg-black text-white hover:text-orange-500">SignUp</button>
+                    <input type="password" name="passwordCheck" value={formData.passwordCheck} onChange={handleChange}
+                           placeholder="비밀번호 확인" className="border p-[1rem] rounded-2xl w-[300px]"/>
                 </div>
+                {emailValid && <div className="m-[2rem]">
+                    <button type="submit"
+                            className="border rounded-[2rem] w-[150px] h-[40px] bg-black text-white hover:text-orange-500">이메일
+                        인증
+                    </button>
+                </div>
+                }
 
+                {nameValid && emailValid && passwordValid && passwordCheckValid && emailCheckValid &&
+                    <div className="m-[2rem]">
+                        <button type="submit"
+                                className="border rounded-[2rem] w-[150px] h-[40px] bg-black text-white hover:text-orange-500">SignUp
+                        </button>
+                    </div>
+                }
             </form>
         </div>
     );
