8f6250b63dc586fe3c004490a35df517b842d317
천영성 <hokoro123@gmail.com>
2025-03-27 19:28:07 +0900

    사이드바 숨기기 생성 버튼 + 동작 구현

--- cheonblogapp/src/Layout/BlogLayout/jsx/Sidebar.jsx
+++ cheonblogapp/src/Layout/BlogLayout/jsx/Sidebar.jsx
@@ -1,25 +1,51 @@
 import React from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 
 function Sidebar(){
+    const [menuButtonCheckValid , setMenuButtonCheckValid] = useState(true);    // 메뉴 클릭 여부 false - > 사이드 바 사라짐 / true -> 사이드 바 생성 
+
+    const handleMenuClick = () =>{
+        if(menuButtonCheckValid === true){
+            setMenuButtonCheckValid(false);
+        }else{
+            setMenuButtonCheckValid(true);
+        }
+    }
+
     return(
-        <div className="md:w-[15rem] flex flex-col bg-[#FAF3E0]">
+        <>
+        {!menuButtonCheckValid && (
+            <div className="mx-[4px] my-[8px] px-[4px] py-[8px]">
+                <span className="material-symbols-outlined hover:scale-125 transition-transform duration-200">
+                    <button type="button" onClick={handleMenuClick}>
+                        double_arrow
+                    </button>
+                </span>
+            </div>
+        )}
+        <div className={`fixed top-0 left-0 h-full bg-[#FAF3E0] transition-transform duration-500 ease-in-out ${menuButtonCheckValid ? "translate-x-0" : "-translate-x-full"} md:w-[15rem] flex flex-col z-20`}>
             <div className="flex items-start m-[8px] gap-[60px]">
                 <div className="px-[4px] py-[8px]">
                     <p className="text-1xl font-bold">hokoro123</p>
                 </div>
                 <div className="px-[4px] py-[8px] flex gap-[10px]">
-                    <span class="material-symbols-outlined hover:scale-125 transition-transform duration-200">
-                        menu
+                    <span className="material-symbols-outlined hover:scale-125 transition-transform duration-200">
+                        <button type="button" onClick={handleMenuClick}>
+                            menu
+                        </button>
                     </span>
-                    <span class="material-symbols-outlined hover:scale-125 transition-transform duration-200">
-                        note_add
+                    <span className="material-symbols-outlined hover:scale-125 transition-transform duration-200">
+                        <button type="button">
+                            note_add
+                        </button>
                     </span>
                 </div>
             </div>
             <hr/>
         </div>
+        </>
     );
 }
 
